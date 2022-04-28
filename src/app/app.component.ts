e188ab0b9bd5623e4abf04cf8b63c64d6de1c8c8
Silvelo <arturo.sivelo@gmail.com>
2022-04-28 15:53:15 +0200

    [fix]: Close sidenav on click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core'
+import { Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  public constructor(private titleService: Title) {
+    this.titleService.setTitle('Silvelo Portfolio')
+  }
+}
