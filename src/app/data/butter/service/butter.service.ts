a333afc685c7166bf2abcebad590e00761437d49
Arturo Silvelo <arturo.silvelo@gmail.com>
2022-04-28 22:43:14 +0200

    [fix]: Fix packages

--- src/app/data/butter/service/butter.service.ts
+++ src/app/data/butter/service/butter.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpParams } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import Butter from 'buttercms'
 import { AwardObject } from '../types/award'
 import { IButterData } from '../types/butter'
 import { CertificationObject } from '../types/certification'
