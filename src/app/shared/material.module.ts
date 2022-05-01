6295acb45daae4dde93e670d99f89f548cb5f54d
Silvelo <arturo.silvelo@gmail.com>
2022-05-01 18:03:42 +0200

    [fix]: Remove icon module

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core'
 import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatIconModule } from '@angular/material/icon'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
@@ -16,7 +15,6 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle'
     MatProgressBarModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule,
     MatSidenavModule,
     MatCardModule,
     MatListModule,
@@ -27,7 +25,6 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle'
     MatProgressBarModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule,
     MatSidenavModule,
     MatCardModule,
     MatListModule,
