b1ad9ecd9e40396bfd48ec029bca658c05a33d98
Silvelo <arturo.silvelo@gmail.com>
2022-12-24 12:36:19 +0100

    refactor: education

--- src/app/modules/education/pages/education/education.component.ts
+++ src/app/modules/education/pages/education/education.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { MatTabChangeEvent } from '@angular/material/tabs'
+import { RouteNames } from '@app/core/common/routes'
 import { Award } from '@app/data/butter/types/award'
 import { Certification } from '@app/data/butter/types/certification'
 import { ButterService } from '@data/butter/service/butter.service'
@@ -16,6 +17,7 @@ export class EducationComponent implements OnInit {
   public educationList: Education[] = []
   public awards: Award[] = []
   public certifications: Certification[] = []
+  public routeNames = RouteNames
 
   constructor(private butterService: ButterService) {}
 
