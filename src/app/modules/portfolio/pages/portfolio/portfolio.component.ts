4738c2a897dad38144e1964d1a4d88e203665f5d
Silvelo <arturo.silvelo@gmail.com>
2022-12-24 12:27:49 +0100

    refactor: portfolio

--- src/app/modules/portfolio/pages/portfolio/portfolio.component.ts
+++ src/app/modules/portfolio/pages/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { RouteNames } from '@app/core/common/routes'
 import { ButterService } from '@app/data/butter/service/butter.service'
 import { Portfolio } from '@app/data/butter/types/portfolio'
 import { firstValueFrom } from 'rxjs'
@@ -10,7 +11,7 @@ import { firstValueFrom } from 'rxjs'
 })
 export class PortfolioComponent implements OnInit {
   public portfolioList: Portfolio[] = []
-
+  public routeNames = RouteNames
   constructor(private butterService: ButterService) {}
 
   async ngOnInit() {
