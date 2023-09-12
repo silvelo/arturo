a3eceefe2260011a40132dbb4c852dcdbae9dae6
Arturo Silvelo <arturo.silvelo@gmail.com>
2023-09-12 17:01:10 +0200

    style: fix break styles

--- src/app/core/components/content-layout/content-layout.component.ts
+++ src/app/core/components/content-layout/content-layout.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'silvelo-content-layout',
   templateUrl: './content-layout.component.html',
   styleUrls: ['./content-layout.component.scss'],
-  host: { class: 'flex flex-col h-screen' },
+  host: { class: 'flex flex-col h-full' },
 })
-export class ContentLayoutComponent {
+export class ContentLayoutComponent implements OnInit {
   isClosed = false;
+
+  ngOnInit(): void {
+    const screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      this.isClosed = true;
+    }
+  }
 
   onToggle() {
     this.isClosed = !this.isClosed;
